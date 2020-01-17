fa130adbba4751aa601f3d0bd75f84bab0f61b8b
Vien Nguyen <42534471+nguyenngocvien@users.noreply.github.com>
2020-01-17 16:58:56 +0700

    update ui

--- app/src/main/java/com/example/vshop/ultil/Server.java
+++ app/src/main/java/com/example/vshop/ultil/Server.java
@@ -1,10 +1,10 @@
 package com.example.vshop.ultil;
 
 public class Server {
-    public static String localhost = "192.168.1.3:8080";
-    public static String duongDanLoaiSp = "http://" + localhost + "/server/getloaisp.php";
-    public static String duongDanSpMoiNhat = "http://" + localhost + "/server/getsanphammoinhat.php";
-    public static String duongDanDienthoai = "http://" + localhost + "/server/getsanpham.php?page=";
-    public static String duongDanDonhang = "http://" + localhost + "/server/thongtinkhachhang.php?";
-    public static String duongDanChitietdonhang = "http://" + localhost + "/server/chitietdonhang.php?";
+    public static String localhost = "https://viennguyen79.000webhostapp.com/Server/";
+    public static String duongDanLoaiSp = localhost + "getloaisp.php";
+    public static String duongDanSpMoiNhat = localhost + "getsanphammoinhat.php";
+    public static String duongDanDienthoai = localhost + "getsanpham.php?page=";
+    public static String duongDanDonhang = localhost + "thongtinkhachhang.php?";
+    public static String duongDanChitietdonhang = localhost + "chitietdonhang.php?";
 }
